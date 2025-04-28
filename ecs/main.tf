97cd279689ab7de88562812d2ae9536e1904fa92
Daniel Ibarrola <daniel.ibarrola.sanchez@gmail.com>
2025-04-28 12:51:09 -0600

    Added vpc endpoints

--- ecs/main.tf
+++ ecs/main.tf
@@ -40,6 +40,14 @@ data "aws_subnets" "ats_public_subnets" {
   }
 }
 
+data "aws_route_tables" "ats_private_rts" {
+  vpc_id = data.aws_vpc.target_vpc.id
+  filter {
+    name   = "tag:Name"
+    values = ["private-route-table"]
+  }
+}
+
 # --- Check if subnets were found ---
 # This will cause Terraform plan/apply to fail if no matching subnets are found
 resource "null_resource" "validate_subnets_found" {
@@ -263,5 +271,75 @@ resource "aws_ecs_service" "ats_client_service" {
   tags = {
     Name        = "ATS Client Service QA"
     Environment = var.environment
+  }
+}
+
+# This SG controls traffic *to* the endpoint ENIs from within your VPC
+resource "aws_security_group" "vpc_endpoint_sg" {
+  name        = "vpc-endpoint-sg-qa"
+  description = "Allow traffic from within VPC to VPC Endpoints"
+  vpc_id      = data.aws_vpc.target_vpc.id
+
+  ingress {
+    description     = "Allow HTTPS from ECS tasks"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ats_client_task_sg.id]
+  }
+
+  # Typically, endpoints don't need egress rules, but allow all if needed for specific scenarios
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "VPC Endpoint SG"
+    Environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id              = data.aws_vpc.target_vpc.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  subnet_ids         = data.aws_subnets.ats_private_subnets.ids
+  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+
+  tags = {
+    Name        = "ECR API Endpoint"
+    Environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id              = data.aws_vpc.target_vpc.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  subnet_ids         = data.aws_subnets.ats_private_subnets.ids
+  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+
+  tags = {
+    Name        = "ECR DKR Endpoint"
+    Environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "s3_gateway" {
+  vpc_id            = data.aws_vpc.target_vpc.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = data.aws_route_tables.ats_private_rts.ids
+
+  tags = {
+    Name = "S3 Gateway Endpoint QA"
   }
 }
